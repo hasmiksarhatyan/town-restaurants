4602f28dfaaa3247e15fce71a85fca833a447ba8
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-12-17 23:53:33 +0400

    RF-18

--- town-restaurants-web/src/main/java/am/itspace/townrestaurantsweb/TownRestaurantsWebApplication.java
+++ town-restaurants-web/src/main/java/am/itspace/townrestaurantsweb/TownRestaurantsWebApplication.java
@@ -1,19 +1,12 @@
 package am.itspace.townrestaurantsweb;
 
-import am.itspace.townrestaurantscommon.entity.Role;
-import am.itspace.townrestaurantscommon.entity.User;
-import am.itspace.townrestaurantscommon.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.util.Optional;
 
 @EnableAsync
 @SpringBootApplication
@@ -26,25 +19,7 @@ import java.util.Optional;
 })
 public class TownRestaurantsWebApplication {
 
-//    private final UserRepository userRepository;
-//    private final PasswordEncoder passwordEncoder;
-
     public static void main(String[] args) {
         SpringApplication.run(TownRestaurantsWebApplication.class, args);
     }
-
-//    @Override
-//    public void run(String... args) throws Exception {
-//        Optional<User> byEmail = userRepository.findByEmail("admin@mail.com");
-//        if (byEmail.isEmpty()) {
-//            userRepository.save(User.builder()
-//                    .firstName("admin")
-//                    .lastName("admin")
-//                    .email("admin@mail.com")
-//                    .password(passwordEncoder.encode("admin1234$"))
-//                    .role(Role.MANAGER)
-//                    .enabled(true)
-//                    .build());
-//        }
-//    }
 }
